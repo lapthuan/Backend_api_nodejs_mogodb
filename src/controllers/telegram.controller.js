616a9eca257d516d743a51598fb46c477101e827
thuan <lapthuan0805@gmail.com>
2023-02-27 12:17:50 +0700

    update

--- src/controllers/telegram.controller.js
+++ src/controllers/telegram.controller.js
@@ -18,6 +18,8 @@ bot.on('message', async (msg) => {
         await dataModel.find({ idtelegram: chatId }).then((data) => {
             console.log(data[0].nhietdo);
             bot.sendMessage(chatId, `Nhiệt đô hiện tại : ${data[0].nhietdo}`);
+        }).catch((error) => {
+            bot.sendMessage(chatId, `Không tìm thấy id chat`);
         })
 
     } else if (text === '/doam') {
@@ -25,22 +27,30 @@ bot.on('message', async (msg) => {
         await dataModel.find({ idtelegram: chatId }).then((data) => {
             console.log(data[0].doam);
             bot.sendMessage(chatId, `Độ ẩm hiện tại : ${data[0].doam}`);
+        }).catch((error) => {
+            bot.sendMessage(chatId, `Không tìm thấy id chat`);
         })
     } else if (text === '/luongnuoc') {
 
         await dataModel.find({ idtelegram: chatId }).then((data) => {
             console.log(data[0].ultrasonic);
             bot.sendMessage(chatId, `Lượng nước hiện tại : ${data[0].ultrasonic}`);
+        }).catch((error) => {
+            bot.sendMessage(chatId, `Không tìm thấy id chat`);
         })
     } else if (text === '/doamdat') {
 
         await dataModel.find({ idtelegram: chatId }).then((data) => {
             console.log(data[0].mhsensor);
             bot.sendMessage(chatId, `Độ ẩm đất hiện tại : ${data[0].mhsensor}`);
+        }).catch((error) => {
+            bot.sendMessage(chatId, `Không tìm thấy id chat`);
         })
     }
-    else {
+    else if(text === '/start'){
         bot.sendMessage(chatId, "Các lệnh \n Lấy dữ liệu nhiệt độ /nhietdo \n Lấy dữ liệu độ ẩm /doam \n Lấy dữ liệu lượng nước /luongnuoc \n Lấy dữ liệu độ ẩm đất /doamdat");
+    }else {
+        bot.sendMessage(chatId,"Xin chào");
     }
 
     return;
